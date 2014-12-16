088a2be8d11117ef7a77847e0f4bf7ba85f8de09
ema-grande <ema.grandem@gmail.com>
2014-12-16 11:23:16 +0100

    use eng and change date of latest with difference time

--- application/views/index.php
+++ application/views/index.php
@@ -1,28 +1,34 @@
-<br/>
+<?php
+	$now = time();
+	$speedDiff = $now - strtotime($listSpeed[0]->time);
+	$discDiff = $now - strtotime($listDrop[0]->time);
+	$pingDiff = $now - strtotime($listPing[0]->time);
+	$format = "z \d H \h i \m\i\\n s \s\\e\c"
+?><br/>
 <div class="container starter-template">
 	<div class="panel panel-default">
-		<div class="panel-heading">Ultimo test speed</div>
+		<div class="panel-heading">Latest speed test</div>
 		<div class="panel-body">
-			L'ultimo speed test del <strong><?php echo $listSpeed[0]->time ?></strong>, 
-			download speed <strong><?php echo $listSpeed[0]->dl ?></strong>, 
-			uplaod speed <strong><?php echo $listSpeed[0]->up ?></strong>,
-			con un ping di <strong><?php echo $listSpeed[0]->ping ?></strong>
+			Latest speed test <strong><?php echo gmdate($format, $speedDiff) ?></strong> ago,
+			with download speed <strong><?php echo $listSpeed[0]->dl ?></strong>,
+			uplaod speed <strong><?php echo $listSpeed[0]->up ?></strong> and
+			ping <strong><?php echo $listSpeed[0]->ping ?></strong>
 		</div>
 	</div>
 	
 	<div class="panel panel-default">
-		<div class="panel-heading">Ultima disconnessione</div>
+		<div class="panel-heading">Latest disconnection</div>
 		<div class="panel-body">
-			L'ultima volta disconnesso risulta il <strong><?php echo $listDrop[0]->time ?></strong>, 
-			duarato <strong><?php echo gmdate('H:i:s', $listDrop[0]->durata); ?></strong>, 
+			Latest disconnection result <strong><?php echo gmdate($format, $discDiff) ?></strong> ago,
+			last for <strong><?php echo gmdate('H:i:s', $listDrop[0]->durata); ?></strong>,
 		</div>
 	</div>
 	
 	<div class="panel panel-default">
 		<div class="panel-heading">Ultimo ping</div>
 		<div class="panel-body">
-			L'ultima ping alto registrato risulta il <strong><?php echo $listPing[0]->time ?></strong>, 
-			di <strong><?php echo $listPing[0]->ping; ?></strong>, 
+			Latest high ping result <strong><?php echo gmdate($format, $pingDiff) ?></strong> ago,
+			of <strong><?php echo $listPing[0]->ping; ?></strong>,
 		</div>
 	</div>
 </div>
