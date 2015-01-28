a5f50b36496391ffde07504dd79c20650534bf6d
ema-grande <ema.grandem@gmail.com>
2015-01-28 16:16:37 +0100

    refactor ping test. Something TODO: avg over 2 ping test

--- script/adsl-drop.sh
+++ script/adsl-drop.sh
@@ -8,32 +8,38 @@ CONF="$(dirname "$0")/conf"
 . $CONF
 
 CONN_FLAG=1		# 1 connect -1 not connect
+				# assume connected at start to avoid
+				# insert record in DB
 
 while [ 1 ]
 do
 	# Every half an hour make speed test
 	MINUTES=$(date +%M)
-	if [ $MINUTES -eq 00 -o $MINUTES -eq 30 ]; then
+	let "MINUTES=$MINUTES%30"
+	if [ $MINUTES -eq 0 ]; then
 		bash $SCRIPT_ROOT/adsl-speed.sh
 		sleep 60		#prevent multi speed test in 1 min
 	fi
-	# change in "sleep until speed test is done" find a way :o
+	# TODO: change in "sleep until speed test is done" find a way :o
 
-	P=$(ping $PING_OPT $TARGET1 | grep -e "time=[0-9]" | sed 's/.*time=//g')
-	ping $PING_OPT $TARGET1 &> /dev/null
+	P1=$(ping $PING_OPT $TARGET1)
 	PING1=$?
-	ping $PING_OPT $TARGET2 &> /dev/null
+	P2=$(ping $PING_OPT $TARGET2)
 	PING2=$?
 	
-	if [ $PING1 -eq 0 ]; then
+	# $TARGETs reachbale? test ping delay
+	if [ $PING1 -eq 0 -a $PING2 -eq 0 ]; then
 		# Record ping if higher then some top latency
-		P_INT=$(echo $P | cut -d" " -f1 | cut -d"." -f1)
+		P1=$(echo $P1 | grep -e "time=[0-9]" | sed 's/.*time=//g' | sed 's/ --- .*//g')
+		P2=$(echo $P2 | grep -e "time=[0-9]" | sed 's/.*time=//g' | sed 's/ --- .*//g')
+		P_INT=$(echo $P1 | cut -d"." -f1)
 		# FIXME: some error may occur if ping fail "integer expression expected"
 		# this should be fixed
 		[[ $P_INT -gt $PING_TOP ]] && mysql -h $DB_HOST -u $DB_USER -e \
 			"INSERT INTO $DB_NAME.$PING_TABLE VALUES (NULL, UNIX_TIMESTAMP(NOW()), \"$P\");" 
 	fi
 	
+	# Control over $TARGETs and $CONN_FLAG
 	if [ $PING1 -eq 0 -a $PING2 -eq 0 -a $CONN_FLAG -ne 1 ]
 	then
 		# ADSL up
