21a736bfe92e61de04a3382a9ed9946e37500bcb
ema-grande <ema.grandem@gmail.com>
2015-01-29 22:51:55 +0100

    replace echo with printf

--- application/views/ping-index.php
+++ application/views/ping-index.php
@@ -21,7 +21,7 @@
 		$diff = $now - $item->time;
 ?>
 		<tr>
-			<td class="id"><?php echo $i ?></td>
+			<td class="id"><?php printf("%d", $i) ?></td>
 		<?php if( $diff < 1800 ) { 
 			$diffH = gmdate("H", $diff);
 			$diffMin = gmdate("i", $diff);
@@ -32,12 +32,12 @@
 					( $diffH != "00" ) ? $time = $time . "$diffH h " : $time;
 					( $diffMin != "00" ) ? $time = $time . "$diffMin min " : $time;
 					$time = $time . "$diffSec sec ago";
-					echo $time;
+					printf("%s", $time);
 				} ?></td>
 		<?php } else { ?>
-			<td class="time"><?php if (isset($item->time)) echo htmlspecialchars(date("Y-m-d H:i:s", $item->time), ENT_QUOTES, 'UTF-8'); ?></td>
+			<td class="time"><?php if (isset($item->time)) printf("%s", htmlspecialchars(date("Y-m-d H:i:s", $item->time), ENT_QUOTES, 'UTF-8')); ?></td>
 		<?php } ?>
-			<td class="ping"><?php if (isset($item->ping)) echo htmlspecialchars($item->ping, ENT_QUOTES, 'UTF-8'); ?></td>
+			<td class="ping"><?php if (isset($item->ping)) printf("%s", htmlspecialchars($item->ping, ENT_QUOTES, 'UTF-8')); ?></td>
 		</tr>
 <?php		$i++;
 	}
